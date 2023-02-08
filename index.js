3abc034f399a34e4ffddb40bec01ed84a10d7c28
Manan Sodha <manants1512@gmail.com>
2023-02-08 12:27:28 +0530

    name change in the main file updated in js name of the changed file updated in the index.js file.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use("/assets", express.static("assets"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", function(req, res){
